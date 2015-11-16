0eb0e4e0b855e037dd5208dafeae3b494f2c3a72
Duncan Zhang <duncanzhang1@gmail.com>
2015-11-15 22:53:37 -0600

    Modified Test Bench

--- testbench.sv
+++ testbench.sv
@@ -6,8 +6,8 @@ timeprecision 1ns;
 
 logic [9:0] BallX, BallY, DrawX, DrawY, Ball_size;
 logic [7:0]  Red, Green, Blue;
-logic [14:0] address;
 logic [31:0] data;
+logic [14:0] i;
 													
 
 
@@ -27,8 +27,14 @@ DrawX = 0;
 DrawY=0;
 Ball_size=0;
 
-#10 address = 16'd139;
-#10 address = 16'd140;
-
+for(i=0; i<26624; i++)
+	begin
+		if((i%128==0) && i!=0) 
+		begin
+			i=0;
+			DrawY++;
+		end
+	#1 DrawX = i;
+	end
 end
 endmodule
